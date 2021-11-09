d3541afd5afe171a00a2748b7e4f0b40e4cc98c4
Lawrence Lee <lawrence@shiru.com>
2021-11-09 11:57:19 -0800

    fix: update icon

--- superset/config.py
+++ superset/config.py
@@ -232,7 +232,8 @@ PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 1, "x_port": 1, "x_prefi
 APP_NAME = "Superset"
 
 # Specify the App icon
-APP_ICON = "/static/assets/images/superset-logo-horiz.png"
+APP_ICON = "/static/assets/images/shiru_logo_green.png"
+APP_ICON_WIDTH = 126
 
 # Specify where clicking the logo would take the user
 # e.g. setting it to '/' would take the user to '/superset/welcome/'
